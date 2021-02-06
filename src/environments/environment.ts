08bc1fce8dd7b0e081e4f8b9b74a263f8f76f7b3
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-02-06 11:33:02 +0300

    add environment.ts to gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "f49b0330c435cca8245cd254327f444ea98af1db"
 };
 
 /*
