8e88916e533bda910874482014e0e9a4bba40fe4
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-02-07 12:29:07 +0300

    add user class

---
+++ src/app/user.ts
@@ -0,0 +1,20 @@
+export class User {
+     constructor(
+        public userInfo:any,
+        public userName:any,
+        public name:any,
+        public avatar_url:any,
+        public login:any,
+        public location:any,
+        public html_url:any,
+        public twitter_username:any,
+        public email:any,
+        public followers:any,
+        public following:any,
+        public public_repos:any,
+        public bio:any,
+        public public_gists:any,
+        public updated_at:any,
+        )
+        {}
+}
