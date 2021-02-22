4133420a7481b915f80ff598b929a28c6792d41b
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-02-22 22:07:08 +0300

    fix error in title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Quotes
+# Github Search
 ### By Dorothy Muhonja
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.1.
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../../github.service';
-import 'rxjs/add/operator/map';
+// import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
